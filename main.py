5bfe243ae12df623b1164d0b1198e39e89cad314
GConscious <amrithgandham@gmail.com>
2023-05-30 17:03:35 -0700

    New change

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 def print_hi(name):
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
 def main():
-    print_hi("Ava")
-    for i in range(5):
-        print("I like coffee")
-    print_hi("collin shen likes J Moon")
+    print('This is the start')
+    parties = 1
+    frat = ['phi pools']
+    print(frat)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
