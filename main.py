1e1bf6eb3598846737091eaba50f0d36aaf2ffe2
collinshen123 <collinshen123@gmail.com>
2023-05-31 14:57:38 -0700

    read csv

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 import pandas as pd
 import flake8
 
+
 def print_hi(name):
-    print(f'Hi, {name}')
-    print('dont say hello to me')
-    print('changes final')
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
 def main():
-    print('A sorority girl on a Tuesday afternoon: ')
-    print('Amrith go get some bitches!!!')
-    for i in range(5):
-        print("I like coffee")
-    print("ᕦ⊙෴⊙ᕤ")
-    parties = 1
-    print('Mykyta is going to which frat?')
-    frat = ['phi pools', 'delts']
-    print(frat)
+    df = pd.read_csv("hate_crime.csv")
+    print(df.head())
+
+
+
 
 
 
