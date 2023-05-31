53fc5a537ae99700a08d8ecaa4e3ad4fc4cdfb8b
collinshen123 <collinshen123@gmail.com>
2023-05-31 15:16:00 -0700

    changes

--- main.py
+++ main.py
@@ -2,23 +2,14 @@ import pandas as pd
 import flake8
 
 
-
 def main():
     df = pd.read_csv("hate_crime.csv")
     print(df)
 
 
 def collin_method():
+    print('hello')
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
