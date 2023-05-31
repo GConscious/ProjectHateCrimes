1104acc6d2d6b61bc6996b04f988e7ac541a2685
GConscious <amrithgandham@gmail.com>
2023-05-31 15:52:12 -0700

    Filter from 2010 to 2021

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import matplotlib as plt
 
 # Line chart for number of crimes committed per year
 def line_chart(df: pd.DataFrame):
-
+    pass
 
 
 def main():
-    df = pd.read_csv("hate_crime.csv")
-    hate_crimes = df.dropna()
-    print(len(hate_crimes['state_name']))
-    # print(collin_method(main()))
+    hate_crimes = pd.read_csv("hate_crime.csv")
+    data = hate_crimes[(hate_crimes['data_year'] >= 2010) & (hate_crimes['data_year'] <= 2021)]
+    # print(collin_method(hate_crimes))
+    print(line_chart(hate_crimes))
 
 
 def collin_method(df: pd.DataFrame):
