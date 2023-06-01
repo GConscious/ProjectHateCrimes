4e4ca89990f35cae90f902fcaf3f66c42bbb81f3
collinshen123 <collinshen123@gmail.com>
2023-06-01 14:19:25 -0700

    collin changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import flake8
 import seaborn as sns
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 
 # Line chart for number of crimes committed per year
@@ -10,16 +10,26 @@ def line_chart(df: pd.DataFrame):
 
 
 def main():
+    df = pd.read_csv("hate_crime.csv")
+    hate_crimes = df.dropna()
+    print(len(hate_crimes['state_name']))
+    collin_method(df)
     hate_crimes = pd.read_csv("hate_crime.csv")
     data = hate_crimes[(hate_crimes['data_year'] >= 2010) & (hate_crimes['data_year'] <= 2021)]
-    # print(collin_method(hate_crimes))
     print(line_chart(hate_crimes))
 
 
 def collin_method(df: pd.DataFrame):
-    collin_data = df[['victim_count', 'bias_desc']].dropna()
-    grouped_data = collin_data.groupby('bias_desc')['victim_count'].sum()
-    return grouped_data
+    collin_data = df[['victim_count', 'offender_race']].dropna()
+    # filtered_data = collin_data[collin_data >= 100]
+
+    sns.catplot(data=collin_data, kind="bar", x="offender_race", y="victim_count")
+    plt.xlabel("Offender's Race")
+    plt.ylabel('Total Victim Count')
+    plt.title('Victim Count by Bias Description')
+
+    # Display the chart
+    plt.show()
 
 
 if __name__ == '__main__':
