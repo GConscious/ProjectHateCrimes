6739e869b0335dd9f4eabc857f9d81b926138a9f
collinshen123 <collinshen123@gmail.com>
2023-06-01 14:56:08 -0700

    collin changes

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import flake8
 import seaborn as sns
 import matplotlib.pyplot as plt
+import plotly.express as px
 
 
 # Line chart for number of crimes committed per year
@@ -12,8 +13,8 @@ def line_chart(df: pd.DataFrame):
 def main():
     hate_crimes = pd.read_csv("hate_crime.csv")
     data = hate_crimes[(hate_crimes['data_year'] >= 2010) & (hate_crimes['data_year'] <= 2021)]
-    collin_method(data)
-    print(collin_method(data))
+    collin_method1(data)
+    collin_method2(data)
 
 
 def collin_method1(df: pd.DataFrame):
@@ -28,6 +29,22 @@ def collin_method1(df: pd.DataFrame):
     plt.title('Victim Count by Offender\'s Race')
     plt.xticks(rotation=90)
     plt.show()
+
+
+def collin_method2(df: pd.DataFrame):
+    collin_data = df[['victim_count', 'data_year']].dropna()
+
+    # Calculate total victim count by year
+    grouped_data = collin_data.groupby('data_year')['victim_count'].sum().reset_index()
+
+    # Create a line plot using Plotly
+    fig = px.line(grouped_data, x='data_year', y='victim_count')
+    fig.update_layout(
+        xaxis_title='Year',
+        yaxis_title='Total Victim Count',
+        title='Victim Count Over the Years'
+    )
+    fig.show()
 
 
 if __name__ == '__main__':
