c852ccb9fbd9d30a2083e3d0b9c251a54f9a7339
GConscious <amrithgandham@gmail.com>
2023-06-01 17:56:46 -0700

    Merged changes

--- main.py
+++ main.py
@@ -6,31 +6,30 @@ import plotly.express as px
 
 
 # Line chart for number of crimes committed per year
-def line_chart(df: pd.DataFrame):
-    pass
 
 
 def main():
     hate_crimes = pd.read_csv("hate_crime.csv")
     data = hate_crimes[(hate_crimes['data_year'] >= 2010) & (hate_crimes['data_year'] <= 2021)]
-    collin_method1(data)
-    collin_method2(data)
-    collin_method3(data)
-    collin_method4(data)
+    # mykyt_method(data)
+    # amrith_map(data)
+    amrith_line_chart(data)
+    # collin_method1(data)
+    # collin_method2(data)
+    #collin_method3(data)
+    # collin_method4(data)
 
 
 def collin_method1(df: pd.DataFrame):
     collin_data = df[['victim_count', 'offender_race']].dropna()
-    grouped_data = collin_data.groupby('offender_race')['victim_count'].mean()
+    grouped_data = collin_data.groupby('offender_race')['victim_count'].sum().reset_index()
 
-    mean_data = pd.DataFrame({'offender_race': grouped_data.index, 'victim_count': grouped_data.values})
+    # Create a bar chart using Plotly
+    fig = px.bar(grouped_data, x='offender_race', y='victim_count',
+                 labels={'victim_count': 'Total Victim Count'},
+                 title='Victim Count by Offender\'s Race')
 
-    sns.barplot(data=mean_data, x='offender_race', y='victim_count')
-    plt.xlabel("Offender's Race")
-    plt.ylabel('Total Victim Count (Thousands)')
-    plt.title('Victim Count by Offender\'s Race')
-    plt.xticks(rotation=90)
-    plt.show()
+    fig.show()
 
 
 def collin_method2(df: pd.DataFrame):
@@ -85,6 +84,30 @@ def collin_method4(df: pd.DataFrame):
     fig.show()
 
 
+def amrith_line_chart(df: pd.DataFrame):
+    df = df[['data_year']].dropna()
+    hate_crime_counts = df.groupby('data_year').size().reset_index(name='crime_count')
+
+    fig = px.line(hate_crime_counts, x='data_year', y='crime_count',
+                  title='Number of Hate Crimes by Year')
+    fig.show()
+
+
+def amrith_map(df: pd.DataFrame):
+    hate_crime_counts = df.groupby('state_abbr').size().reset_index(name='count')
+
+    fig = px.choropleth(
+        hate_crime_counts,
+        locations='state_abbr',
+        locationmode='USA-states',
+        color='count',
+        scope='usa',
+        labels={'count': 'Number of Victims'},
+        title='Number of Hate Crimes by State',
+        color_continuous_scale='Blues'
+    )
+
+    fig.show()
 
 
 # sum of victim count across us by state since 2010
@@ -107,6 +130,7 @@ def mykyt_method(df: pd.DataFrame) -> None:
         width=800
     )
     fig.show()
+
 
 if __name__ == '__main__':
     main()
